1ab396695c516797850562dea8683f43d38ea841
ysalman962 <yusefsalman19@gmail.com>
2026-09-16 17:14:14 +0300

    Update connection string name to DefaultConnection

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("BookingDb")
-    ?? throw new InvalidOperationException("Connection string 'BookingDb' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 
 
 /*------------------------------ Builder services ------------------------------*/
